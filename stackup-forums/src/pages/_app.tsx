40358d34c85c24dd37941915bd6c1781a3247004
Soc Virnyl Estela <contact@uncomfyhalomacro.pl>
2024-09-03 11:16:09 +0800

    minor fixes and changes

--- stackup-forums/src/pages/_app.tsx
+++ stackup-forums/src/pages/_app.tsx
@@ -1,6 +1,9 @@
 import "../styles/globals.css";
 import "@rainbow-me/rainbowkit/styles.css";
 import type { AppProps } from "next/app";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config as fontawesomeConfig } from "@fortawesome/fontawesome-svg-core";
+fontawesomeConfig.autoAddCss = false;
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { WagmiProvider } from "wagmi";
