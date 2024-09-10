d9c3605f0424ef553013da3b34fa67c4f1f213e3
Soc Virnyl S. Estela <contact@uncomfyhalomacro.pl>
2024-09-10 13:23:51 +0800

    V2 (#2)
    * change heart arrangements
    
    * heart should not be inside link

--- stackup-forums/src/pages/index.tsx
+++ stackup-forums/src/pages/index.tsx
@@ -42,6 +42,7 @@ const Home: NextPage = () => (
 				<a href="https://stackup.dev" target="_blank" rel="noreferrer">
 					StackUp ❤️
 				</a>{" "}
+				❤️{" "}
 				in partnership with{" "}
 				<a href="https://arbitrum.io" rel="noreferrer" target="_blank">
 					Arbitrum!
